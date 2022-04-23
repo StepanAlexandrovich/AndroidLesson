a5dd6f56d2a29db4837342de024dbc84ac2b467b
stepan <fisenko.stepan@mail.ru>
2022-04-23 20:29:07 +0300

    lesson_1_2

--- app/src/main/java/java/android/androidlesson/MainActivity.java
+++ app/src/main/java/java/android/androidlesson/MainActivity.java
@@ -2,7 +2,11 @@ package java.android.androidlesson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
