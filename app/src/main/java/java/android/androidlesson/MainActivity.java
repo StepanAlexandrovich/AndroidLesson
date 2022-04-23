0dcf8133ae126f7b6544109cfefb45035fe04267
stepan <fisenko.stepan@mail.ru>
2022-04-23 21:21:44 +0300

    lesson_1_3

--- app/src/main/java/java/android/androidlesson/MainActivity.java
+++ app/src/main/java/java/android/androidlesson/MainActivity.java
@@ -8,20 +8,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button buttonMainActivity2;
+    private Button buttonCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
-                startActivity(intent);
-            }
-        });
+        buttonMainActivity2 = findViewById(R.id.buttonMainActivity2);
+        buttonCalendar = findViewById(R.id.buttonCalendar);
+
+        buttonMainActivity2.setOnClickListener(this);
+        buttonCalendar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button)view;
+        if(button == buttonMainActivity2){
+            startActivity(new Intent(MainActivity.this,MainActivity2.class));
+        }else
+        if(button == buttonCalendar){
+            startActivity(new Intent(MainActivity.this,Calendar.class));
+        }
     }
 }
